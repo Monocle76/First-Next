23edf46262e0485a48b136b54b6812cf527a93c9
Monocle76 <monocle76@proton.me>
2023-01-17 11:32:17 +0530

    Removed Dependancy

--- src/pages/apihello.js
+++ src/pages/apihello.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Container from "./components/Container";
 
 export async function getServerSideProps() {
